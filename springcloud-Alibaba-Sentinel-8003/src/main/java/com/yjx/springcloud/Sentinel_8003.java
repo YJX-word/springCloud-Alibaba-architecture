a8d520127705a4eaf55abb7f37209db0d27c2d81
YJX-word <1767178984@qq.com>
2022-04-18 21:43:54 +0800

    update ignore

--- springcloud-Alibaba-Sentinel-8003/src/main/java/com/yjx/springcloud/Sentinel_8003.java
+++ springcloud-Alibaba-Sentinel-8003/src/main/java/com/yjx/springcloud/Sentinel_8003.java
@@ -1,6 +1,9 @@
 package com.yjx.springcloud;
 
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
+import com.alibaba.csp.sentinel.slots.block.degrade.circuitbreaker.CircuitBreakerStrategy;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.boot.SpringApplication;
@@ -22,17 +25,41 @@ import java.util.List;
 public class Sentinel_8003 {
     public static void main(String[] args) {
         initRule("resource");
+        initDegradeRule("resource");
         SpringApplication.run(Sentinel_8003.class,args);
     }
-    static void initRule(String str){
-        List<FlowRule> rules = new ArrayList<FlowRule>();
+    private static void initRule(String str){
+        List<FlowRule> rules = new ArrayList<>();
         FlowRule rule = new FlowRule();
         rule.setResource(str);
-// set limit qps to 10
-        rule.setCount(20);
+        // set limit qps to 10
+        rule.setCount(2);
         rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        rule.setControlBehavior(RuleConstant.CONTROL_BEHAVIOR_DEFAULT);
+//        rule.setControlBehavior(RuleConstant.CONTROL_BEHAVIOR_RATE_LIMITER);
+//        rule.setMaxQueueingTimeMs(20 * 1000); //设置等待时间
         rule.setLimitApp("default");
         rules.add(rule);
         FlowRuleManager.loadRules(rules);
+        System.out.println("FlowRule rule loaded: " + rules);
     }
+
+    private static void initDegradeRule(String str) {  //熔断规则
+        List<DegradeRule> rules = new ArrayList<>();
+        DegradeRule rule = new DegradeRule(str)
+                .setGrade(CircuitBreakerStrategy.SLOW_REQUEST_RATIO.getType())
+                // Max allowed response time
+                .setCount(5000) //对于慢调用策略时 单位为ms
+                // Retry timeout (in second)
+                .setTimeWindow(10) //设置熔断时长
+                // Circuit breaker opens when slow request ratio > 60%
+                .setSlowRatioThreshold(0.6)
+                .setMinRequestAmount(10)  //最小的异常请求数量
+                .setStatIntervalMs(20000); //统计时长
+        rules.add(rule);
+
+        DegradeRuleManager.loadRules(rules);
+        System.out.println("Degrade rule loaded: " + rules);
+    }
+
 }
--- springcloud-Alibaba-Sentinel-8003/src/main/java/com/yjx/springcloud/controller/DeptContrller.java
+++ springcloud-Alibaba-Sentinel-8003/src/main/java/com/yjx/springcloud/controller/DeptContrller.java
@@ -1,9 +1,11 @@
 package com.yjx.springcloud.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.AbstractRule;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.yjx.springcloud.pojo.Dept;
 import com.yjx.springcloud.service.DeptService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @Author: Yjx
@@ -39,21 +42,30 @@ public class DeptContrller {
         return deptService.queryById(id);
     }
 
+    @SneakyThrows
     @GetMapping("/dept/list")
-    @SentinelResource(value = "resource",fallback = "blockqueryALl",blockHandler = "exceptionQueryAll")
+    //fallback可以处理所有的异常除了（exceptionsToIgnore排除的异常），而bockHandler只能处理blockException定点异常
+    @SentinelResource(value = "resource",fallback = "blockQueryAll",blockHandler = "exceptionQueryAll")
     public List<Dept> queryALl(){
+        Random random = new Random();
+//        Thread.sleep(4000);
+        if(random.nextInt(3)==0){
+            System.out.println("出现了自定义的异常");
+            throw new Exception("出现随机数异常");
+        }
         return deptService.queryALl();
     }
-
-    public List<Dept> blockqueryALl(){
+    //处理其他任何异常
+    public List<Dept> blockQueryAll(Throwable e){
         List<Dept> list = new ArrayList<>();
-        Dept dept = new Dept().setDeptno(1L).setDname("没有数据").setDb_source("数据库为空");
+        Dept dept = new Dept().setDeptno(1L).setDname(e.getMessage()).setDb_source("数据库为空");
         list.add(dept);
         return list;
     }
+    //熔断和流量控制的等sentinel中包含的异常都会经过该方法，则在方法中需要自行判断属于什么异常
     public List<Dept> exceptionQueryAll(BlockException blockException){
         List<Dept> list = new ArrayList<>();
-        Dept dept = new Dept().setDeptno(1L).setDname(blockException.getMessage()).setDb_source("数据库为空");
+        Dept dept = new Dept().setDeptno(1L).setDname(String.valueOf(blockException.getRule())).setDb_source("出现异常，数据库为空");
         list.add(dept);
         return list;
     }
